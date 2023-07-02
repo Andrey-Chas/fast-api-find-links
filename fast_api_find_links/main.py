5cd5f2c01165969fe543046c33cf9dbbe215df33
Andrey Chasovskoy <andrchas777@gmail.com>
2023-07-02 22:04:06 +0300

    Files updated

--- fast_api_find_links/main.py
+++ fast_api_find_links/main.py
@@ -1,21 +1,8 @@
 from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
 from fast_api_find_links.some_text import SomeText
 from fast_api_find_links.find_meeting_links import *
 
 app = FastAPI()
-
-origins = [
-    "http://127.0.0.1:8081"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # links = []
 # head = "http://127.0.0.1:8081/find_links"
